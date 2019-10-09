4e11ac8804da683f8054596f03ca292e45fc3691
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-10-09 15:58:43 -0700

    cleaned up code with handleChange

--- src/StudentForm.js
+++ src/StudentForm.js
@@ -14,6 +14,7 @@ class _StudentForm extends Component{
       error: ''
     }
     this.create = this.create.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   async create(){
     try {
@@ -23,22 +24,22 @@ class _StudentForm extends Component{
       this.setState({ error: ex.response.data.message })
     }
   }
-  // handleChange = (ev)=> {
-  //   this.setState({
-  //     [ev.target.name]: ev.target.value
-  //   })
-  // }
+  handleChange(ev){
+    this.setState({
+      [ev.target.name]: ev.target.value
+    })
+  }
 
   render(){
     //console.log('schools', this.props.schools)
-    const { create } = this;
+    const { create, handleChange } = this;
     return (
       <form className='studentForm' onSubmit={ (ev)=> ev.preventDefault() }>
-        First Name <input type='text' name='firstName' onChange={ (ev)=> this.setState({ firstName: ev.target.value }) }></input><br></br>
-        Last Name <input type='text' name='lastName' onChange={ (ev)=> this.setState({ lastName: ev.target.value })}></input><br></br>
-        Email <input type='email' name='email' onChange={ (ev)=> this.setState({ email: ev.target.value })}></input><br></br>
-        GPA <input type='number' step='0.01' name='GPA' onChange={ (ev)=> this.setState({ GPA: ev.target.value })}></input><br></br>
-        Enroll at: <select name='schoolId' onChange={ (ev)=> this.setState({ schoolId: ev.target.value })}>
+        First Name <input type='text' name='firstName' onChange={ (ev)=> handleChange(ev) }></input><br></br>
+        Last Name <input type='text' name='lastName' onChange={ (ev)=> handleChange(ev) }></input><br></br>
+        Email <input type='email' name='email' onChange={ (ev)=> handleChange(ev) }></input><br></br>
+        GPA <input type='number' step='0.01' name='GPA' onChange={ (ev)=> handleChange(ev) }></input><br></br>
+        Enroll at: <select name='schoolId' onChange={ (ev)=> handleChange(ev) }>
           <option value='notEnrolled'>--Not Enrolled--</option>
           {
             this.props.schools.map( school => <option key={school.id} value={ school.id}>{ school.name }</option>)
@@ -57,7 +58,7 @@ const mapStateToProps = ({ schools, students })=> {
   }
 }
 
-const mapDispatchToProps = (dispatch, getState)=> {
+const mapDispatchToProps = (dispatch)=> {
   return {
     create: (student)=> dispatch(addStudent(student))
   };
