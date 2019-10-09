2e4ff1af1576548b0563be7117ddb3f5f18b91ab
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-10-08 17:45:14 -0700

    added students and made all fields not null

--- db.js
+++ db.js
@@ -26,8 +26,14 @@ const Student = conn.define("student", {
     type: STRING,
     allowNull: false
   },
-  email: STRING,
-  GPA: DECIMAL,
+  email: {
+    type: STRING,
+    allowNull: false
+  },
+  GPA: {
+    type: DECIMAL,
+    allowNull: false
+  },
   error: STRING
 });
 
@@ -50,7 +56,9 @@ const syncAndSeed = async()=> {
     { firstName: 'Nicole', lastName: 'Consolacion', email: 'nikkiconsolacion@gmail.com', GPA: 4.0, schoolId: calpoly.id },
     { firstName: 'Foo', lastName: 'Bar', email: 'foobar@gmail.com', GPA: 3.2, schoolId: notredame.id },
     { firstName: 'Bazz', lastName: 'Quq', email: 'bazzquq@gmail.com', GPA: 3.1, schoolId: standford.id },
-    { firstName: 'Tommy', lastName: 'Pickles', email: 'tpickles@calpoly.edu', schoolId: calpoly.id}
+    { firstName: 'Tommy', lastName: 'Pickles', email: 'tpickles@calpoly.edu', GPA: 3.5, schoolId: calpoly.id},
+    { firstName: 'Chandler', lastName: 'Bing', email: 'misschanandlerbong@gmail.com', GPA: 3.7, schoolId: harvard.id },
+    { firstName: 'Phoebe', lastName: 'Buffet', email: 'pheebs@gmail.com', GPA: 2.5}
   ];
   await Promise.all(students.map( student => Student.create(student)));
 };
