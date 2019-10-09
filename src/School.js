888bb55ff6430138d7fa048e802968d63c5645ae
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-10-08 17:19:29 -0700

    generated count for students enrolled

--- src/School.js
+++ src/School.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Schools from './Schools';
 
-const _School = ({ schools, match })=> {
+const _School = ({ schools, students, match })=> {
   const schoolId = match.params.id
   let school;
 
@@ -15,13 +15,13 @@ const _School = ({ schools, match })=> {
     school = schools.find( school => school.id === schoolId);
     return (
       <div>
-        <h3>{school.name}</h3>
+        <h3>{school.name} ({ students.filter( student => student.schoolId === schoolId).length } Students enrolled)</h3>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ schools })=> ({ schools });
+const mapStateToProps = ({ schools, students })=> ({ schools, students });
 
 const School = connect(mapStateToProps)(_School);
 
